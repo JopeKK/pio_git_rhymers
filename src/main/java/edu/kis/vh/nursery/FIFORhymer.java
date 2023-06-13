df0d3d021c8abc9a49d3ecf35c0ffd2d5068cc09
JopeKK <w.j.jopkiewicz@gmail.com>
2023-06-13 18:59:41 +0200

    Poprawka 3 - poprawki w pliku FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -4,16 +4,18 @@ public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    public DefaultCountingOutRhymer getTemp() {
+        return temp;
+    }
+
     @Override
     public int countOut() {
         while (!callCheck())
-
             temp.countIn(super.countOut());
 
         int ret = temp.countOut();
 
         while (!temp.callCheck())
-
             countIn(temp.countOut());
 
         return ret;
